69b830137d73b1e94d0179bbe5596d76f803dc0a
Saydul Islam <saydulislamshojul@gmail.com>
2025-05-22 00:53:05 +0600

    slick slide added

---
+++ assets/js/main.js
@@ -0,0 +1,20 @@
+$(document).ready(function(){
+  $('.testimonial-items').slick({
+  dots: true,
+  infinite: true,
+  speed: 300,
+  slidesToShow: 2,
+  slidesToScroll: 1,
+  autoplay: true,
+  autoplaySpeed: 2000,
+  responsive: [
+    {
+      breakpoint: 767,
+      settings: {
+        slidesToShow: 1,
+        slidesToScroll: 1
+      }
+    }
+  ]
+});
+})
