3034b2b0cc7d4795b2e36076b4d4ccae29db78c4
Saydul Islam <saydulislamshojul@gmail.com>
2025-05-22 20:40:51 +0600

    subscribe area done

--- assets/js/main.js
+++ assets/js/main.js
@@ -5,8 +5,8 @@ $(document).ready(function(){
   speed: 300,
   slidesToShow: 2,
   slidesToScroll: 1,
-  // autoplay: true,
-  // autoplaySpeed: 2000,
+  autoplay: true,
+  autoplaySpeed: 2000,
   responsive: [
     {
       breakpoint: 767,
