ba712ff533998e1a83c76b7e7993d5a8306a0784
Saydul Islam <saydulislamshojul@gmail.com>
2025-05-25 17:25:42 +0600

    fixed and complete the blog area

--- assets/js/main.js
+++ assets/js/main.js
@@ -2,14 +2,46 @@ $(document).ready(function(){
   $('.testimonial-items').slick({
   dots: true,
   infinite: true,
-  speed: 300,
+  speed: 1500,
   slidesToShow: 2,
   slidesToScroll: 1,
   autoplay: true,
-  autoplaySpeed: 2000,
+  autoplaySpeed: 3000,
   responsive: [
     {
-      breakpoint: 767,
+      breakpoint: 992,
+      settings: {
+        slidesToShow: 1,
+        slidesToScroll: 1
+      }
+    }
+  ]
+});
+
+$('.gallery-image-link').magnificPopup({
+  type: 'image',
+  // other options
+});
+
+$('.blog-items').slick({
+  dots: true,
+  infinite: true,
+  speed: 1500,
+  slidesToShow: 3,
+  slidesToScroll: 1,
+  autoplay: true,
+  autoplaySpeed: 3000,
+  responsive: [
+    {
+      breakpoint: 992,
+      settings: {
+        slidesToShow: 2,
+        slidesToScroll: 1
+      }
+    },
+
+    {
+      breakpoint: 768,
       settings: {
         slidesToShow: 1,
         slidesToScroll: 1
