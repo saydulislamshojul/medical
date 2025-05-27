06c78721faaca3205b07c93f7bc9344c4ede15bf
Saydul Islam <saydulislamshojul@gmail.com>
2025-05-27 19:06:03 +0600

    fixed the issues

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,52 +1,73 @@
-$(document).ready(function(){
-  $('.testimonial-items').slick({
-  dots: true,
-  infinite: true,
-  speed: 1500,
-  slidesToShow: 2,
-  slidesToScroll: 1,
-  autoplay: true,
-  autoplaySpeed: 3000,
-  responsive: [
-    {
-      breakpoint: 992,
-      settings: {
-        slidesToShow: 1,
-        slidesToScroll: 1
-      }
-    }
-  ]
-});
+(function ($) {    
+    "use strict"
 
-$('.gallery-image-link').magnificPopup({
-  type: 'image',
-  // other options
-});
+  $(document).ready(function(){
 
-$('.blog-items').slick({
-  dots: true,
-  infinite: true,
-  speed: 1500,
-  slidesToShow: 3,
-  slidesToScroll: 1,
-  autoplay: true,
-  autoplaySpeed: 3000,
-  responsive: [
-    {
-      breakpoint: 992,
-      settings: {
-        slidesToShow: 2,
-        slidesToScroll: 1
-      }
-    },
+    // testimonial slider
+    $('.testimonial-slider').slick({
+      dots: true,
+      infinite: true,
+      speed: 1500,
+      slidesToShow: 2,
+      slidesToScroll: 1,
+      autoplay: true,
+      autoplaySpeed: 3000,
+      responsive: [
+        {
+          breakpoint: 992,
+          settings: {
+            slidesToShow: 1,
+            slidesToScroll: 1
+          }
+        }
+      ]
+    });
 
-    {
-      breakpoint: 768,
-      settings: {
-        slidesToShow: 1,
-        slidesToScroll: 1
+    // gallery popup
+    $('.hover-state a').magnificPopup({
+      type: 'image',
+      gallery:{
+        enabled: true
       }
-    }
-  ]
-});
-})
+      // other options
+    });
+
+    // blog slider
+    $('.blog-slider').slick({
+      dots: true,
+      infinite: true,
+      speed: 1500,
+      slidesToShow: 3,
+      slidesToScroll: 1,
+      autoplay: true,
+      autoplaySpeed: 3000,
+      responsive: [
+        {
+          breakpoint: 992,
+          settings: {
+            slidesToShow: 2,
+            slidesToScroll: 1
+          }
+        },
+
+        {
+          breakpoint: 768,
+          settings: {
+            slidesToShow: 1,
+            slidesToScroll: 1
+          }
+        }
+      ]
+    })
+
+  })
+
+  $(window).on('load', function () {
+
+      // preloader
+      var preLoder = $("#preloader");
+      preLoder.fadeOut(0);
+
+  });
+
+})(jQuery);
